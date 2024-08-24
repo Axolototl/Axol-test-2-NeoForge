9f46608aa72034d871c9739a1f3b9277669c427e
Axolototl <jdavidvargasc26@gmail.com>
2024-08-23 19:53:19 -0500

    Module 1 - Final

--- src/main/java/net/axolototl/axoltest2/datagen/ModBlockStateProvider.java
+++ src/main/java/net/axolototl/axoltest2/datagen/ModBlockStateProvider.java
@@ -9,6 +9,8 @@ import net.neoforged.neoforge.client.model.generators.ModelFile;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.registries.DeferredBlock;
 
+import java.awt.*;
+
 public class ModBlockStateProvider extends BlockStateProvider {
     public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
         super(output, AxolTest2.MOD_ID, exFileHelper);
@@ -18,39 +20,36 @@ public class ModBlockStateProvider extends BlockStateProvider {
     protected void registerStatesAndModels() {
         blockWithItem(ModBlocks.BLACK_OPAL_BLOCK);
         blockWithItem(ModBlocks.RAW_BLACK_OPAL_BLOCK);
+
         blockWithItem(ModBlocks.BLACK_OPAL_ORE);
-        blockWithItem(ModBlocks.BLACK_OPAL_DEEPSLATE_ORE);
-        blockWithItem(ModBlocks.BLACK_OPAL_NETHER_ORE);
+        blockWithItem(ModBlocks.BLACK_OPAL_DEEPSLATE_ORE );
         blockWithItem(ModBlocks.BLACK_OPAL_END_ORE);
+        blockWithItem(ModBlocks.BLACK_OPAL_NETHER_ORE);
 
         blockWithItem(ModBlocks.MAGIC_BLOCK);
 
         stairsBlock(((StairBlock) ModBlocks.BLACK_OPAL_STAIRS.get()), blockTexture(ModBlocks.BLACK_OPAL_BLOCK.get()));
         slabBlock(((SlabBlock) ModBlocks.BLACK_OPAL_SLAB.get()), blockTexture(ModBlocks.BLACK_OPAL_BLOCK.get()), blockTexture(ModBlocks.BLACK_OPAL_BLOCK.get()));
 
-        blockItem(ModBlocks.BLACK_OPAL_SLAB);
-        blockItem(ModBlocks.BLACK_OPAL_STAIRS);
-
         pressurePlateBlock(((PressurePlateBlock) ModBlocks.BLACK_OPAL_PRESSURE_PLATE.get()), blockTexture(ModBlocks.BLACK_OPAL_BLOCK.get()));
-        buttonBlock(((ButtonBlock) ModBlocks.BLACK_OPAL_BLOCK.get()), blockTexture(ModBlocks.BLACK_OPAL_BLOCK.get()));
-
-        blockItem(ModBlocks.BLACK_OPAL_PRESSURE_PLATE);
-
+        buttonBlock(((ButtonBlock) ModBlocks.BLACK_OPAL_BUTTON.get()), blockTexture(ModBlocks.BLACK_OPAL_BLOCK.get()));
 
         fenceBlock(((FenceBlock) ModBlocks.BLACK_OPAL_FENCE.get()), blockTexture(ModBlocks.BLACK_OPAL_BLOCK.get()));
-        fenceGateBlock((((FenceGateBlock) ModBlocks.BLACK_OPAL_FENCE_GATE.get())), blockTexture(ModBlocks.BLACK_OPAL_BLOCK.get()));
+        fenceGateBlock(((FenceGateBlock) ModBlocks.BLACK_OPAL_FENCE_GATE.get()), blockTexture(ModBlocks.BLACK_OPAL_BLOCK.get()));
         wallBlock(((WallBlock) ModBlocks.BLACK_OPAL_WALL.get()), blockTexture(ModBlocks.BLACK_OPAL_BLOCK.get()));
 
-        blockItem(ModBlocks.BLACK_OPAL_FENCE_GATE);
-
-        doorBlockWithRenderType(((DoorBlock) ModBlocks.BLACK_OPAL_DOOR.get()), modLoc("block/black_opal_door_bottom"),
-                modLoc("block/black_opal_door_top"), "cutout");
+        doorBlockWithRenderType(((DoorBlock) ModBlocks.BLACK_OPAL_DOOR.get()), modLoc("block/black_opal_door_bottom"), modLoc("block/black_opal_door_top"), "cutout");
         trapdoorBlockWithRenderType(((TrapDoorBlock) ModBlocks.BLACK_OPAL_TRAPDOOR.get()), modLoc("block/black_opal_trapdoor"), true, "cutout");
+
+        blockItem(ModBlocks.BLACK_OPAL_STAIRS);
+        blockItem(ModBlocks.BLACK_OPAL_SLAB);
+        blockItem(ModBlocks.BLACK_OPAL_PRESSURE_PLATE);
+        blockItem(ModBlocks.BLACK_OPAL_FENCE_GATE);
 
         blockItem(ModBlocks.BLACK_OPAL_TRAPDOOR, "_bottom");
     }
 
-    private void blockWithItem(DeferredBlock<Block> deferredBlock){
+    private void blockWithItem(DeferredBlock<Block> deferredBlock) {
         simpleBlockWithItem(deferredBlock.get(), cubeAll(deferredBlock.get()));
     }
 
